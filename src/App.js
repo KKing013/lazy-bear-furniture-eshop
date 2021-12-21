844c9ef887d78bcf9cddc25e939fe26e721b4964
Kevin King <kjking013@live.com>
2021-12-21 16:15:34 +0200

    Sidebar component, navbar and footer
    Added sidebar
    Added navbar
    Added footer
    Added cart buttons
    Setup router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
+import {Home, Products, SingleProduct, About, Cart, Error, Checkout, PrivateRoute} from './pages'
+
+
 function App() {
-  return <h4>lazy bear furniture</h4>
+
+  return (
+
+  <Router>
+
+    <Navbar />
+    <Sidebar />
+
+    
+    <Routes>
+
+    <Route exact path='/' element={<Home />} />
+    <Route exact path='/about' element={<About />} />
+    <Route exact path='/cart' element={<Cart />} />
+    <Route exact path='/products' element={<Products />} />
+    <Route exact path='/products/:id' element={<SingleProduct />} />
+    <Route exact path='/checkout' element={<Checkout />} />
+    <Route path='*' element={<Error />} />
+
+
+
+    </Routes>
+
+    <Footer />
+  
+
+  </Router>
+
+  )
+    
+  
 }
 
 export default App
